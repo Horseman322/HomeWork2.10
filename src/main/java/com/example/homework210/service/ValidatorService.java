e0b8c67ea32d24dca29d0c415f3bb94489ab2b86
Kosarev <kosarevn97@gmail.com>
2022-09-13 21:47:48 +0700

    Initial commit

---
+++ src/main/java/com/example/homework210/service/ValidatorService.java
@@ -0,0 +1,11 @@
+package service;
+
+import org.springframework.util.StringUtils;
+
+public class ValidatorService {
+
+    public String validateName(String name){
+        if (StringUtils)
+    }
+
+}
